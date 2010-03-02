57b9e59cd819bd7324221cfed134461db4c95571
kengroenn <kengroenn@1cd60dd2-38a1-11de-96ab-f16d9912eac9>
2010-03-02 15:57:39 +0000

    ¨sda

--- src/ovingT4/PersonPanelList.java
+++ src/ovingT4/PersonPanelList.java
@@ -5,8 +5,6 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.Action;
-import javax.swing.ButtonGroup;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
